9ec8b802d4b0780d0882a9a34fec466429aff874
Muhammad Kumail <mkumailofficial@gmail.com>
2021-05-23 05:03:53 +0500

    google analytics integration

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./assests/font-awesome/css/all.css";
+import ReactGA from "react-ga";
+
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const engine = new Styletron();
 
