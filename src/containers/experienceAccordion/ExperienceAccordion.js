98ddefe8ffd7b054e5bfdf4ccc4d8c79f29493ad
Muhammad Kumail <mkumailofficial@gmail.com>
2021-07-08 04:12:38 +0500

    react-build fix

--- src/containers/experienceAccordion/ExperienceAccordion.js
+++ src/containers/experienceAccordion/ExperienceAccordion.js
@@ -16,9 +16,13 @@ class ExperienceAccordion extends Component {
                 title={section["title"]}
                 key={section["title"]}
               >
-                {section["experiences"].map((experience) => {
+                {section["experiences"].map((experience, key) => {
                   return (
-                    <ExperienceCard experience={experience} theme={theme} />
+                    <ExperienceCard
+                      key={key}
+                      experience={experience}
+                      theme={theme}
+                    />
                   );
                 })}
               </Panel>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import { BaseProvider, LightTheme } from "baseui";
 import { Provider as StyletronProvider } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
