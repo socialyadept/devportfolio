a04ba650d07881f146ee65fbd6397bba2526cd1c
Muhammad Kumail <mkumailofficial@gmail.com>
2021-07-07 21:42:32 +0500

    fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import ReactDOM from "react-dom";
 import { BaseProvider, LightTheme } from "baseui";
 import { Provider as StyletronProvider } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
