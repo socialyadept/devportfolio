7c6cec81b99cfa108acf0c962c152d7dd897d9fa
Muhammad Kumail <mkumailofficial@gmail.com>
2025-02-13 16:33:50 -0600

    feat: 404 page - under progress

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+import styles from "styles/404.module.css";
+
+const Education = (props) => {
+  const theme = props.theme;
+  return <div className={styles["not-found-main"]}>Page not found</div>;
+};
+
+export default Education;
